63a2e792652e39d253cacef9732b40fa5a01dc5c
RajDalvi08 <raj.233054101@vcet.edu.in>
2026-07-05 21:24:05 +0530

    feat: implement user login and JWT/cookie handling on registration

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -35,9 +35,75 @@ async function registerUserController(req, res){
         password: hash
     })
 
-    res.status(201).json({message: "User registered successfully", user: user})
+    const token = jwt.sign(
+        {id: user._id, username: user.username}, 
+        process.env.JWT_SECRET, 
+        {expiresIn: "1d"}
+    )
+
+    res.cookie("token", token)
+
+    
+
+    res.status(201).json({
+        message: "User registered successfully", 
+        user:{
+            id: user._id,
+            username: user.username,
+            email: user.email
+        },
+       
+    })
+}
+
+/**
+ * @name loginUserController
+ * @description Login a user, expects email and password in the request body. It checks if the user exists, compares the provided password with the hashed password in the database, and returns a JWT token if successful.
+ * @access Public
+ */
+async function loginUserController(req, res){
+ const {email, password} = req.body;
+
+ const user = await userModel.findOne({email})
+
+ if(!user){
+    return res.status(400).json({
+        message:"Invalid email or password"
+    })
+
+ }
+
+ const isPasswordValid= bcrypt.compare(password, user.password)
+
+ if(!isPasswordValid){
+    return res.status(400).json({
+        message:"Invalid email or password"
+    })
+ }
+
+     const token = jwt.sign(
+        {id: user._id, username: user.username}, 
+        process.env.JWT_SECRET, 
+        {expiresIn: "1d"}
+    )
+
+
+
+
+    res.cookie("token", token)
+    res.status(200).json({
+        message:"User loggedIn successfully",
+          user:{
+            id: user._id,
+            username: user.username,
+            email: user.email
+        },
+    })
+
+
 }
 
 module.exports ={
-    registerUserController
+    registerUserController,
+    loginUserController
 }
